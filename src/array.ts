1c3c9b7cf5b9d5365cd1d6e08f64b5d52c1953c9
chunseop <357609318@qq.com>
2022-11-18 18:27:53 +0800

    left join, inner join

--- src/array.ts
+++ src/array.ts
@@ -28,3 +28,145 @@ export function arrangeObjArray(
 function defaultCompare(oldObj: any, newObj: any) {
 	return oldObj.id == newObj.id
 }
+
+export interface IRelFields {
+	leftField: string
+	rightField: string
+}
+
+/**
+ * leftJoin
+ * @param rows1
+ * @param rows2
+ * @param rels
+ * @param rows2Fields
+ * @param fillEmptyField
+ * @returns
+ */
+export function leftJoin(
+	rows1: Array<any>,
+	rows2: Array<any>,
+	rels: Array<IRelFields>,
+	rows2Fields: Array<string>,
+	fillEmptyField = true
+): Array<any> {
+	let results = []
+	for (let idx1 in rows1) {
+		let row1 = rows1[idx1]
+
+		let noHit = true
+		for (let idx2 in rows2) {
+			let hit = true
+			let row2 = rows2[idx2]
+			for (let relIdx in rels) {
+				const rel = rels[relIdx]
+				if (row1[rel.leftField] != row2[rel.rightField]) {
+					hit = false
+					break
+				}
+			}
+
+			if (hit) {
+				noHit = false
+				const joinRow = {
+					...row1
+				}
+				Object.keys(row2).forEach((key: string) => {
+					if (!joinRow[key]) {
+						joinRow[key] = row2[key]
+					}
+				})
+
+				results.push(joinRow)
+			}
+		}
+
+		if (noHit) {
+			const joinRow = { ...row1 }
+			if (fillEmptyField) {
+				rows2Fields.forEach((field) => {
+					if (!joinRow[field]) {
+						joinRow[field] = null
+					}
+				})
+			}
+
+			results.push(joinRow)
+		}
+	}
+
+	return results
+}
+
+/**
+ * innerJoin
+ * @param rows1
+ * @param rows2
+ * @param rels
+ * @returns
+ */
+export function innerJoin(
+	rows1: Array<any>,
+	rows2: Array<any>,
+	rels: Array<IRelFields>
+): Array<any> {
+	let results = []
+	for (let idx1 in rows1) {
+		let row1 = rows1[idx1]
+		for (let idx2 in rows2) {
+			let row2 = rows2[idx2]
+			let hit = true
+			for (let relIdx in rels) {
+				const rel = rels[relIdx]
+				if (row1[rel.leftField] != row2[rel.rightField]) {
+					hit = false
+					break
+				}
+			}
+
+			if (hit) {
+				const joinRow = { ...row1 }
+				Object.keys(rows2[0]).forEach((key) => {
+					if (!joinRow[key]) {
+						joinRow[key] = row2[key]
+					}
+				})
+				results.push(joinRow)
+			}
+		}
+	}
+
+	return results
+}
+
+export function subJoin(
+	rows1: Array<any>,
+	rows2: Array<any>,
+	rels: Array<IRelFields>
+): Array<any> {
+	let results = []
+	for (let idx1 in rows1) {
+		let row1 = rows1[idx1]
+		for (let idx2 in rows2) {
+			let row2 = rows2[idx2]
+			let hit = true
+			for (let relIdx in rels) {
+				const rel = rels[relIdx]
+				if (row1[rel.leftField] != row2[rel.rightField]) {
+					hit = false
+					break
+				}
+			}
+			if (hit) {
+				const joinRow = { ...row1 }
+				Object.keys(rows2[0]).forEach((key) => {
+					if (!joinRow[key]) {
+						joinRow[key] = row2[key]
+					}
+				})
+				results.push(joinRow)
+			}
+		}
+	}
+	return results
+}
