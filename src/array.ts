379dfd0439ff52896566e4b4c2cb1ad16b06b7c3
chunseop <357609318@qq.com>
2022-11-25 23:02:52 +0900

    Update array.ts

--- src/array.ts
+++ src/array.ts
@@ -47,10 +47,16 @@ export function leftJoin(
 	rows1: Array<any>,
 	rows2: Array<any>,
 	rels: Array<IRelFields>,
-	rows2Fields: Array<string>,
-	fillEmptyField = true
+	fillEmptyField = true,
+	rows2Fields?: Array<string>
 ): Array<any> {
 	let results = []
+	let buildRow2Fields = false
+	if (!rows2Fields) {
+		buildRow2Fields = true
+		rows2Fields = [] as string[]
+	}
+
 	for (let idx1 in rows1) {
 		let row1 = rows1[idx1]
 
@@ -58,6 +64,14 @@ export function leftJoin(
 		for (let idx2 in rows2) {
 			let hit = true
 			let row2 = rows2[idx2]
+			if (buildRow2Fields) {
+				Object.keys(row2).forEach((colKey) => {
+					if (!rows2Fields?.includes(colKey)) {
+						rows2Fields?.push(colKey)
+					}
+				})
+			}
+
 			for (let relIdx in rels) {
 				const rel = rels[relIdx]
 				if (row1[rel.leftField] != row2[rel.rightField]) {
@@ -140,33 +154,72 @@ export function innerJoin(
 }
 
 export function subJoin(
-	rows1: Array<any>,
-	rows2: Array<any>,
-	rels: Array<IRelFields>
+	mainRows: Array<any>,
+	subRows: Array<any>,
+	rels: Array<IRelFields>,
+	subItemsKey = 'children'
 ): Array<any> {
-	let results = []
-	for (let idx1 in rows1) {
-		let row1 = rows1[idx1]
-		for (let idx2 in rows2) {
-			let row2 = rows2[idx2]
+	for (let mainIdx in mainRows) {
+		let mainRow = mainRows[mainIdx]
+		let children = []
+		for (let subIdx in subRows) {
+			let subRow = subRows[subIdx]
 			let hit = true
 			for (let relIdx in rels) {
 				const rel = rels[relIdx]
-				if (row1[rel.leftField] != row2[rel.rightField]) {
+				if (mainRow[rel.leftField] != subRow[rel.rightField]) {
 					hit = false
 					break
 				}
 			}
+
 			if (hit) {
-				const joinRow = { ...row1 }
-				Object.keys(rows2[0]).forEach((key) => {
-					if (!joinRow[key]) {
-						joinRow[key] = row2[key]
-					}
-				})
-				results.push(joinRow)
+				children.push(subRow)
 			}
 		}
+
+		mainRow[subItemsKey] = children
 	}
-	return results
+
+	return mainRows
+}
+
+/**
+ *  change one row array to name, value Object array
+ * @param arr
+ * @returns
+ */
+export function buildNameValueObjectArray(data: Array<any> | Object): any {
+	if (!data) {
+		return []
+	}
+
+	let nvObjArr = [] as Array<any>
+	let orgObj = {} as Object
+	if (Array.isArray(data)) {
+		if (data.length == 0) {
+			return []
+		}
+
+		// just one row
+		orgObj = data[0]
+	} else {
+		orgObj = data
+	}
+
+	Object.keys(orgObj).forEach((key) => {
+		const nvObj = {} as any
+		nvObj['name'] = key
+		// @ts-ignore
+		nvObj['value'] = orgObj[key]
+
+		nvObjArr.push(nvObj)
+	})
+
+	// // nvObj
+	// {
+	// 	name: '',
+	// 	desc: ''
+	// }
+	return nvObjArr
 }
