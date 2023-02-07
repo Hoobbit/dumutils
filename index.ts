3f14d3e942cf5d29f513fdbac3cfa5c778d6f792
chunseop <357609318@qq.com>
2023-02-08 00:59:55 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,5 +2,7 @@ export * from './src/string'
 export * from './src/array'
 export * from './src/velocity'
 export * from './src/file'
+export * from './src/dimention'
+export * from './src/object'
 
 // export default dumutils
