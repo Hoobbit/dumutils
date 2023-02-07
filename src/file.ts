6103e5bb0143c7f661b12e2caa796b782316147b
chunseop <357609318@qq.com>
2023-02-08 01:00:00 +0800

    Update file.ts

--- src/file.ts
+++ src/file.ts
@@ -1,8 +1,8 @@
-import fs from 'node:fs'
+import fs from 'fs'
 import path from 'path'
 
 interface IFileInfo {
-	fileName: string
+  fileName: string
 }
 
 /**
@@ -10,26 +10,26 @@ interface IFileInfo {
  * @returns
  */
 export function viewDirectoryContents(
-	directoryPath: string,
-	fileFullName = false
+  directoryPath: string,
+  fileFullName = false
 ): {
-	items: Array<IFileInfo>
-	total: number
+  items: Array<IFileInfo>
+  total: number
 } {
-	directoryPath = buildFileFullPath(directoryPath)
-	fs.accessSync(directoryPath, fs.constants.R_OK)
+  directoryPath = buildFileFullPath(directoryPath)
+  fs.accessSync(directoryPath, fs.constants.R_OK)
 
-	const fileNames = fs.readdirSync(directoryPath)
-	let result = [] as any[]
-	fileNames.forEach((fileName) => {
-		if (fileFullName) {
-			fileName = buildFileFullPath(directoryPath, fileName)
-		}
+  const fileNames = fs.readdirSync(directoryPath)
+  let result = [] as any[]
+  fileNames.forEach((fileName) => {
+    if (fileFullName) {
+      fileName = buildFileFullPath(directoryPath, fileName)
+    }
 
-		result.push({ fileName })
-	})
+    result.push({ fileName })
+  })
 
-	return { items: result, total: fileNames.length }
+  return { items: result, total: fileNames.length }
 }
 
 /**
@@ -37,10 +37,10 @@ export function viewDirectoryContents(
  * @param fileName
  */
 export function deleteFile(filePath: string, fileName: string) {
-	const fileFullName = buildFileFullPath(filePath, fileName)
+  const fileFullName = buildFileFullPath(filePath, fileName)
 
-	fs.accessSync(fileFullName, fs.constants.W_OK)
-	fs.unlinkSync(fileFullName)
+  fs.accessSync(fileFullName, fs.constants.W_OK)
+  fs.unlinkSync(fileFullName)
 }
 
 /**
@@ -49,14 +49,14 @@ export function deleteFile(filePath: string, fileName: string) {
  * @param fileName
  */
 export function softDeleteFile(filePath: string, fileName: string) {
-	let oldFileFullName = `${filePath}/${fileName}`
-	let newFileFullName = `${filePath}/deleted_${fileName}`
-	if (process.platform == 'win32') {
-		oldFileFullName = oldFileFullName.split(path.posix.sep).join(path.sep)
-	}
+  let oldFileFullName = `${filePath}/${fileName}`
+  let newFileFullName = `${filePath}/deleted_${fileName}`
+  if (process.platform == 'win32') {
+    oldFileFullName = oldFileFullName.split(path.posix.sep).join(path.sep)
+  }
 
-	fs.accessSync(oldFileFullName, fs.constants.W_OK)
-	fs.renameSync(oldFileFullName, newFileFullName)
+  fs.accessSync(oldFileFullName, fs.constants.W_OK)
+  fs.renameSync(oldFileFullName, newFileFullName)
 }
 
 /**
@@ -65,17 +65,17 @@ export function softDeleteFile(filePath: string, fileName: string) {
  * @param fileData
  */
 export function createFile(
-	filePath: string,
-	fileName: string,
-	fileData: string | NodeJS.ArrayBufferView
+  filePath: string,
+  fileName: string,
+  fileData: string | NodeJS.ArrayBufferView
 ) {
-	filePath = buildFileFullPath(filePath)
-	if (!fs.existsSync(filePath)) {
-		fs.mkdirSync(filePath, { recursive: true })
-	}
+  filePath = buildFileFullPath(filePath)
+  if (!fs.existsSync(filePath)) {
+    fs.mkdirSync(filePath, { recursive: true })
+  }
 
-	const fileFullName = buildFileFullPath(filePath, fileName)
-	fs.writeFileSync(fileFullName, fileData)
+  const fileFullName = buildFileFullPath(filePath, fileName)
+  fs.writeFileSync(fileFullName, fileData)
 }
 
 /**
@@ -84,17 +84,17 @@ export function createFile(
  * @returns
  */
 export function systemPath(filePath: string, sysType?: string): string {
-	const platform = sysType || process.platform
+  const platform = sysType || process.platform
 
-	let systemPath = path.resolve(filePath)
-	if (platform == 'win32') {
-		// windows check
-		systemPath = systemPath.split(path.posix.sep).join(path.win32.sep)
-	} else {
-		systemPath = systemPath.split(path.win32.sep).join(path.posix.sep)
-	}
+  let systemPath = path.resolve(filePath)
+  if (platform == 'win32') {
+    // windows check
+    systemPath = systemPath.split(path.posix.sep).join(path.win32.sep)
+  } else {
+    systemPath = systemPath.split(path.win32.sep).join(path.posix.sep)
+  }
 
-	return systemPath
+  return systemPath
 }
 
 /**
@@ -103,11 +103,11 @@ export function systemPath(filePath: string, sysType?: string): string {
  * @returns fileFullName
  */
 function buildFileFullPath(filePath: string, fileName?: string): string {
-	let fileFullPath = path.resolve(filePath, fileName || '')
-	if (process.platform == 'win32') {
-		// windows check
-		fileFullPath = fileFullPath.split(path.posix.sep).join(path.sep)
-	}
+  let fileFullPath = path.resolve(filePath, fileName || '')
+  if (process.platform == 'win32') {
+    // windows check
+    fileFullPath = fileFullPath.split(path.posix.sep).join(path.sep)
+  }
 
-	return fileFullPath
+  return fileFullPath
 }
