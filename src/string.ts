370c6013ca17d2498377fdda69a7b4e1c7f56b4f
chunseop <357609318@qq.com>
2023-02-08 01:00:06 +0800

    Update string.ts

--- src/string.ts
+++ src/string.ts
@@ -8,23 +8,23 @@ import { customAlphabet } from 'nanoid'
  * @returns
  */
 export function randomString(
-	length = 16,
-	alphabet = '0123456789abcdefghijklmnopqrstuvwxyz',
-	dashSeperator = true
+  length = 16,
+  alphabet = '0123456789abcdefghijklmnopqrstuvwxyz',
+  dashSeperator = true
 ): string {
-	// default params generate: pdlerqxd1rc-67k5
-	const nanoid = customAlphabet(alphabet, length)
-	let rs = nanoid()
+  // default params generate: pdlerqxd1rc-67k5
+  const ni = customAlphabet(alphabet, length)
+  let rs = ni()
 
-	if (length > 8 && dashSeperator) {
-		const tailStartIdx = rs.length - 5
-		return `${rs.substring(0, tailStartIdx)}-${rs.substring(
-			tailStartIdx,
-			length - 1
-		)}`
-	} else {
-		return rs
-	}
+  if (length > 8 && dashSeperator) {
+    const tailStartIdx = rs.length - 5
+    return `${rs.substring(0, tailStartIdx)}-${rs.substring(
+      tailStartIdx,
+      length - 1
+    )}`
+  } else {
+    return rs
+  }
 }
 
 /**
@@ -35,22 +35,22 @@ export function randomString(
  * @returns
  */
 export function randomSerial(
-	length = 6,
-	alphabet = '0123456789abcdef',
-	dashSeperator = true
+  length = 6,
+  alphabet = '0123456789abcdef',
+  dashSeperator = true
 ): string {
-	const nanoid = customAlphabet(alphabet, length)
-	let serial = nanoid()
+  const nid = customAlphabet(alphabet, length)
+  let serial = nid()
 
-	if (length > 8 && dashSeperator) {
-		const tailStartIdx = serial.length - 5
-		return `${serial.substring(0, tailStartIdx)}-${serial.substring(
-			tailStartIdx,
-			length - 1
-		)}`
-	} else {
-		return serial
-	}
+  if (length > 8 && dashSeperator) {
+    const tailStartIdx = serial.length - 5
+    return `${serial.substring(0, tailStartIdx)}-${serial.substring(
+      tailStartIdx,
+      length - 1
+    )}`
+  } else {
+    return serial
+  }
 }
 
 // console.log('randomString', randomString(8))
